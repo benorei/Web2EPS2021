74d03d02301baa734ebc71f0c3fc2c7a46415060
everestoreizy <everest@oreizy.com>
2021-12-04 00:27:26 -0800

    Last Change to Responsive Design

--- voidrobotics/js/nav.js
+++ voidrobotics/js/nav.js
@@ -1,11 +1,5 @@
-let nav_elements = [
-    ["Home", "."],
-    ["Products", "/products"],
-    ["Contact", "/contact"]
-]
-
 let nav = `
-<span class="sub title">Void Robotics</span>
+<span class="title">Void Robotics</span>
 <a class="sub opt" href=".">Home</a>
 <a class="sub opt" href="products">Products</a>
 <a class="sub opt" href="contact">Contact</a>
@@ -18,18 +12,14 @@ let source = `
 <a class="sub-r opt" href="https://github.com/everestoreizy/everestoreizy.github.io/tree/master/voidrobotics">Source</a>
 `
 
-let temp = nav;
-
 //If the website is being hosted on GitHub pages or being run off my computer, show the "Directory" option. This prevents the directory option from being shown while it's hosted on the void.noallus.nl domain where that page isn't available.
 //If the website is being hosted on GitHub pages, show the link to the source code.
 if ( window.location.hostname == "everestoreizy.github.io" ) {
-    temp += directory;
-    temp += source;
+    nav += directory;
+    nav += source;
 }
 
-console.log(temp);
-
-document.getElementsByClassName('nav')[0].innerHTML = temp;
+document.getElementsByClassName('nav')[0].innerHTML = nav;
 
 let title = document.getElementsByTagName("title")[0].innerText;
 
@@ -37,7 +27,6 @@ let nav_items = document.getElementsByClassName("opt");
 
 for (let index = 0; index < nav_items.length; index++) {
     let text = nav_items[index].innerText;
-    console.log(text);
     if(title.startsWith(text)){
         nav_items[index].classList.add("active");
     }

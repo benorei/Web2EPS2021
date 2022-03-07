34441b7936aa5cac87ab08fa6535ce8b7d1e6e4a
everestoreizy <everest@oreizy.com>
2022-03-06 17:43:56 -0800

    stupid portfolio urlopening animations tht took 3h

---
+++ portfolio/js/portfolio.js
@@ -0,0 +1,101 @@
+let portfolio = document.getElementById("portfolio");
+let lastupdated = document.getElementById("lastupdated");
+let popsurface = document.getElementById("portfolio-popout");
+let closebtn = document.getElementById("portfolio-pop-close");
+
+lastupdated.innerText = "Last updated: " + projects.lastupdated;
+
+// Sort (will make options later)
+web = projects.web.sort(sortyr);
+
+
+
+// Populate the portfolio
+web.forEach(item => {
+    let el = document.createElement("div");
+    el.classList.add("fg");
+    el.classList.add("portfolio-item");
+    el.classList.add("cp3");
+
+    //heading
+    let header = document.createElement("h3");
+    header.innerText = item.name;
+
+    //description
+    let description = document.createElement("div");
+    description.classList.add("portfolio-description");
+    description.classList.add("hbox");
+
+    let blurb = document.createElement("p");
+    blurb.innerText = item.description;
+
+    let type_yr = document.createElement("p");
+    type_yr.classList.add("portfolio-type-yr");
+    type_yr.innerText = item.type.toUpperCase() + " (" + item.year + ")";
+
+    description.appendChild(blurb);
+    description.appendChild(type_yr);
+
+    // description.innerText = item.type + " ● " + item.description + " ● " + item.year;
+
+    //year
+    let year = document.createElement("p");
+    year.classList.add("portfolio-year");
+    year.innerText = item.year;
+
+    //image
+    let image = document.createElement("img");
+    image.src = item.imageurl;
+    image.classList.add("portfolio-image");
+
+    //add everything to the div
+    el.appendChild(header);
+    el.appendChild(description);
+    el.appendChild(image);
+
+    //add onclick to div
+    el.onclick = function () {
+        pop(el, item);
+    }
+
+    //add the div to the portfolio
+    portfolio.appendChild(el);
+});
+
+//sort by year
+function sortyr(one, two) {
+    if (one.year < two.year) {
+        return -1;
+    } else if (one.year > two.year) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+let elrect = null;
+
+//toggle large view
+function pop(element, item) {
+    popsurface.style.display = "block";
+    popsurface.src = item.url;
+
+    //have to use settimeout, otehrwise it interrupts the transitions.
+    setTimeout(() => popsurface.classList.add("displayed"), 100);
+    
+    
+    document.body.style.overflow = "hidden";
+
+    //display the close button as well
+    closebtn.classList.add("displayed");
+}
+
+function endpop() {
+    popsurface.classList.remove("displayed");
+    closebtn.classList.remove("displayed");
+    document.body.style.overflow = "auto";
+    // popsurface.style.display = "none";
+    setTimeout(() => popsurface.style.display = "none", 600);
+}
+
+popsurface.style.display = "none"; //set just for the first time
---
+++ portfolio/js/projects.js
@@ -0,0 +1,113 @@
+let projects = {
+    lastupdated: "4 March 2022",
+    web: [
+        {
+            name: "Arch Linux",
+            type: "redesign",
+            url: "../arch_redesign/index.html",
+            description: "Homepage overhaul for Arch Linux",
+            imageurl: "images/projclip/projclip5.png",
+            year: "2022"
+        },
+        {
+            name: "My Pet's Vet Clinic Website",
+            type: "redesign",
+            url: "https://eastsidepreparatoryschool.github.io/web2-group3/index.html",
+            description: "6-page client-directed design",
+            imageurl: "images/projclip/projclip11.png",
+            year: "2021"
+        },
+        {
+            name: "Loading Your Experience",
+            type: "bite",
+            url: "../css_transition/index.html",
+            description: "CSS transitions",
+            imageurl: "images/projclip/projclip4.png",
+            year: "2022"
+
+        },
+        {
+            name: "Frosted Glass",
+            type: "bite",
+            url: "../frostedglass_demo/index.html",
+            description: "Aesthetic testing",
+            imageurl: "images/projclip/projclip3.png",
+            year: "2022"
+        },
+        {
+            name: "Void Robotics '21",
+            type: "custom",
+            url: "../voidrobotics/index.html",
+            description: "What if Void was a robotics vendor?",
+            imageurl: "images/projclip/projclip10.png",
+            year: "2021"
+        },
+        {
+            name: "KrugerSolutions.biz",
+            type: "custom",
+            url: "https://krugersolutions.biz",
+            description: "Physics study resource website",
+            imageurl: "images/projclip/projclip9.png",
+            year: "2021"
+        },
+        {
+            name: "All About Gradients",
+            type: "demo",
+            url: "../gradients_demo/index.html",
+            description: "CSS gradients tutorial",
+            imageurl: "images/projclip/projclip2.png",
+            year: "2022"
+        },
+        {
+            name: "NASA Welcome",
+            type: "redesign",
+            url: "../themedpage/index.html",
+            description: "Winter-themed landing page",
+            imageurl: "images/projclip/projclip8.png",
+            year: "2021"
+        },
+        {
+            name: "Bestsite",
+            type: "bite",
+            url: "../nav_include_demo/index.html",
+            description: "Global nav test",
+            imageurl: "images/projclip/projclip7.png",
+            year: "2021"
+        },
+        {
+            name: "La bolsa nueva",
+            type: "example",
+            url: "../responsive_css/index.html",
+            description: "Responsive CSS design",
+            imageurl: "images/projclip/projclip6.png",
+            year: "2021"
+        },
+        // {
+        //     name: "La bolsa nueva 2.0",
+        //     type: "example",
+        //     url: "../sticky_header/index.html",
+        //     description: "Sticky header modification of 'La bolsa nueva'",
+        //     imageurl: "images/projclip/projclip1.png",
+        //     year: "2022"
+        // },
+        {
+            name: "Odyssey Soundtrack Player",
+            type: "custom",
+            url: "https://morning-ridge-58923.herokuapp.com/",
+            description: "Rehearsal resource for singers",
+            imageurl: "images/projclip/projclip13.png",
+            year: "2017"
+        },
+        {
+            name: "ROLLLINK",
+            type: "custom",
+            url: "https://rolllink.noallus.nl",
+            description: "A meme link shortener",
+            imageurl: "images/projclip/projclip12.png",
+            year: "2020"
+        }
+    ],
+    "java": [
+
+    ]
+}
